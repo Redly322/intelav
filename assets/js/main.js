927d6f5dccf7d7a92e8b58fedea20fd7c9ef2284
Ilya Zakharov <125558874+Redly322@users.noreply.github.com>
2026-07-28 13:59:28 +0300

    Fix to-top button to scroll to absolute page top

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,4 +84,9 @@
     if (form instanceof HTMLFormElement) form.reset();
     return false;
   };
+
+  document.getElementById("to-top")?.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
 })();
